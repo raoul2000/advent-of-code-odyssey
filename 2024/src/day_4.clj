4604e6cc04068c19aab6bd40622cc3cec1fb0811
raoul2000 <manu34@free.fr>
2024-12-06 10:43:27 +0100

    day_4 : solved ⭐

--- 2024/src/day_4.clj
+++ 2024/src/day_4.clj
@@ -44,11 +44,11 @@ MXMXAXMASX")
 (defn parse-input
   "Read string s and returns a grid of char"
   [s]
-  (->> (s/split-lines sample-input)
+  (->> (s/split-lines s)
        (mapv vec)))
 
 (defn char-at [grid [x y]]
-  (get-in grid [x y]))
+  (get-in grid [y x]))
 
 (defn size
   "Returns [col-count line-count] for the given grid"
@@ -62,103 +62,48 @@ MXMXAXMASX")
     (and (< -1 x col-count)
          (< -1 y line-count))))
 
-(comment
 
-  (def g (parse-input sample-input))
-  (char-at g [0 0])
-  (char-at g [0 2])
-  (char-at g [0 3])
-  (char-at g [1 1])
-
-  (size g)
-
-  ;; loop over lines pos
-  (let [[col-count line-count] (size g)]
-    (for [x (range 0 col-count)
-          y (range 0 line-count)]
-      [x y]))
+(defn create-segment
+  "Given a grid and a seq of [x y] pos, return a seq of chars at pos"
+  [grid pos-xs]
+  (apply str (mapv #(char-at grid %) pos-xs)))
 
 
-  ;; loop over cols
-  (let [[col-count line-count] (size g)]
-    (for [x (range 0 col-count)
-          y (range 0 line-count)]
-      [y 0]))
-
-  ;; loop over diagonals top-left -> bottom-right
-  ;; all segment starting points
-  (let [[col-count line-count] (size g)]
-    (for [y (range (dec line-count) -1 -1)]
-      [0 y]))
-
-  (let [[col-count line-count] (size g)]
-    (for [x (range 1 col-count)]
-      [x 0]))
-
-  ;; all diagonal\ segment starting pos 
-  (let [[col-count line-count] (size g)]
-    (apply conj
-           (mapv #(vector 0 %) (range 0 line-count))   ;; right vertical
-           (mapv #(vector % 0) (range 1 col-count))))  ;; top horiz
-
-  ;; all diagonal /  segment starting pos   
-  (let [[col-count line-count] (size g)]
-    (apply conj
-           (mapv #(vector % (dec line-count)) (range 0 col-count))    ;; bottom horiz
-           (mapv #(vector (dec col-count) %)  (range 0 line-count)))) ;; right vertical
-
-
-  ;; given a diagonal staring pos find all pos in the segment
+(defn pos-segment
+  "create segment starting at `[x y]` and for the given `dx` `dy` variations"
+  [grid [x y] dx dy]
   (->> (map (fn [delta]
-              [(+ 0 delta) (+ 0 delta)])  (range 0 20))
-       (take-while #(in-grid? g %)))
+              [(+ x (* dx delta))  (+ y (* dy delta))])  (range))
+       (take-while #(in-grid? grid %))))
 
-  /
-  (defn create-segment-backslash
-    "create all pos for segment \\ starting at `[x y]`"
-    [grid [x y]]
-    (->> (map (fn [delta]
-                [(+ x delta) (+ y delta)])  (range))
-         (take-while #(in-grid? grid %))))
+(defn count-xmas [s]
+  (+ (count (re-seq #"XMAS" s))
+     (count (re-seq #"SAMX" s))))
 
-  (create-segment-backslash g [0 0])
-  (create-segment-backslash g [0 1])
-  (create-segment-backslash g [5 0])
-  (create-segment-backslash g [10 0])
+(defn solution-1 []
+  (let [grid                   (parse-input (slurp "resources/day_4.txt"))
+        [col-count line-count] (size grid)
+        max-y                  (dec line-count)
 
-  (defn create-segment-slash
-    "create all pos for segment / starting at `[x y]`"
-    [grid [x y]]
-    (->> (map (fn [delta]
-                [(- x delta) (+ y delta)])  (range))
-         (take-while #(in-grid? grid %))))
+        dy1 (mapv (fn [y]
+                    (pos-segment grid [0 y] +1 +1)) (range 0 line-count))
+        dx1 (mapv (fn [x]
+                    (pos-segment grid [x 0] +1 +1)) (range 0 col-count))
+        dy2 (mapv (fn [y]
+                    (pos-segment grid [0 y] +1 -1)) (range 0 line-count))
+        dx2 (mapv (fn [x]
+                    (pos-segment grid [x max-y] +1 -1)) (range 0 col-count))
+        hor (mapv (fn [y]
+                    (pos-segment grid [0 y] +1 0)) (range 0 line-count))
+        ver (mapv (fn [x]
+                    (pos-segment grid [x 0] 0 +1)) (range 0 col-count))]
 
-  (create-segment-slash g [3 0])
-  (create-segment-slash g [3 0])
+    (->> (concat dy1 dx1 dy2 dx2 hor ver)
+         (filter #(< 2 (count %)))
+         (map #(create-segment grid %))
+         set
+         (map count-xmas)
+         (apply +))))
 
-
-  (let [[col-count line-count] (size g)]
-    (mapv #(vector 0 %) (range 0 line-count)))
-
-
-  (defn create-segment
-    "Given a grid and a seq of [x y] pos, return a seq of chars at pos"
-    [grid pos-xs]
-    (apply str (mapv #(char-at grid %) pos-xs)))
-
-  (defn count-xmas [s]
-    (+ (count (re-seq #"XMAS" s))
-       (count (re-seq #"SAMX" s))))
-
-  (count-xmas "..XMAS..XM..XMAS..SAMX..XMAS")
-  ;;
-  )
-
-(defn segments-backslash-starting-pos
-  "all diagonal \\ segment starting pos "
-  [grid]
-  (let [[col-count line-count] (size grid)]
-    (apply conj
-           (mapv #(vector 0 %) (range 0 line-count))   ;; right vertical
-           (mapv #(vector % 0) (range 1 col-count))))  ;; top horiz
-  )
+(solution-1)
+;; => 2358 ⭐
