6cdb3a578cd0c36dcea5a862a31dd39a2c67104a
raoul2000 <manu34@free.fr>
2024-12-17 17:50:36 +0100

    day_7 : solved ⭐

---
+++ 2024/src/day_7.clj
@@ -0,0 +1,82 @@
+(ns day-7
+  (:require [clojure.string :as s]
+            [clojure.edn :as edn]))
+
+
+;; https://adventofcode.com/2024/day/7
+
+;; part 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; - evaluated left-to-right
+;; - operators : + *
+
+(def sample-input "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+")
+
+(def puzzle-input (slurp "resources/day_7.txt"))
+
+(defn parse-input [s]
+  (->>
+   (s/split-lines s)
+   (map #(s/split % #"[: ]"))
+   (map #(remove (partial = "") %))
+   ;; use edn/read-string instead of Integer/parseInt
+   (map #(map (fn [n] (edn/read-string n)) %))))
+
+(comment
+  (parse-input sample-input)
+  (parse-input puzzle-input)
+  (Integer/parseInt "1455501866546")
+  (edn/read-string "1455501866546")
+;;
+  )
+
+(defn reduce-computation [target-result num-xs]
+  (reduce (fn [res n]
+            (let [new-result (->> res
+                                  (map (juxt (partial + n) (partial * n)))
+                                  flatten
+                                  (remove #(> % target-result)))]
+              (if (empty? new-result)
+                (reduced false)
+                new-result)))
+          [(first num-xs)]
+          (rest num-xs)))
+
+(comment
+  (reduce-computation 292 [11 6 16 20])
+  ;;
+  )
+
+(defn solution-1 [input]
+  (->> input
+       parse-input
+       (map (fn [n-xs]
+              (let [target-n (first n-xs)
+                    operands (rest n-xs)
+                    computations (->> (reduce-computation target-n operands)
+                                      (some (partial = target-n)))]
+                (when computations target-n))))
+       (remove nil?)
+       (apply +)))
+
+(comment
+  (solution-1 sample-input)
+  ;; => 3749 (good)
+
+  (solution-1 puzzle-input)
+  ;; => 7579994664753 ⭐
+
+  ;;
+  )
+
+
+
