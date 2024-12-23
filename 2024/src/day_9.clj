efd0f27534e219bdce7402a6c6f2f382a165a76a
raoul2000 <manu34@free.fr>
2024-12-23 12:33:20 +0100

    day_9 : refactor solution 1

--- 2024/src/day_9.clj
+++ 2024/src/day_9.clj
@@ -11,57 +11,84 @@
 
 (def puzzle-input (slurp "resources/day_9.txt"))
 
+(defn build-blocks
+  "Given `ìnput`, returns a vector of blocks, where each block is a number 'n' : 
+    - *n* = -1 : free space block
+    - *n* = n : file-id *n* block
+  "
+  [input]
+  (:blocks (reduce-kv (fn [res index c]
+                        (let [block-length  (- (int c) 48)
+                              id-number     (:cur-id res)]
+                          (if (even? index)
+                            ;; dealing with file length
+                            (-> res
+                                (update :cur-id inc)
+                                (update :blocks into (repeat block-length id-number)))
+                            ;; dealing with free space
+                            (-> res
+                                (update :blocks into (repeat  block-length -1))))))
+                      {:cur-id 0
+                       :blocks []}
+                      (vec input))))
+
 (comment
+  (build-blocks sample-input)
+  (build-blocks puzzle-input)
+  ;;
+  )
 
-  (def blocks (:blocks (reduce-kv (fn [res index c]
-                                    (let [block-length  (- (int c) 48)
-                                          id-number     (:cur-id res)]
-                                      (if (even? index)
-                       ;; dealing with file length
-                                        (-> res
-                                            (update :cur-id inc)
-                                            (update :blocks into (repeat block-length id-number)))
-                       ;; dealing with free space
-                                        (-> res
-                                            (update :blocks into (repeat  block-length -1))))))
-                                  {:cur-id 0
-                                   :blocks []}
-                                  (vec puzzle-input))))
+(defn build-indexed-blocks
+  "Given a vector of blocks, returns a seq of pairs where :
+  - first item is the block index
+  - second is the block type (-1 or file-id')"
+  [block-xs]
+  (map-indexed vector block-xs))
 
+(def empty-space? #(= -1 (second %)))
+(def block-index first)
 
-  ;; add index
-  (def indexed-blocks (map-indexed vector blocks))
+(defn build-moveable-blocks [indexed-blocks]
+  (->> indexed-blocks
+       (remove empty-space?)
+       reverse))
 
-  (def empty-space? #(= -1 (second %)))
-  ;; blocks to move
-  (def moveable-blocks (->> indexed-blocks
-                            (remove empty-space?)
-                            reverse))
+(defn defragment-indexed-blocks [indexed-blocks]
+  (loop [blocks          indexed-blocks
+         moveable-blocks (build-moveable-blocks indexed-blocks)
+         final           []]
+    (if (or (empty? moveable-blocks)
+            (< (block-index (first moveable-blocks))
+               (block-index (first blocks))))
+      final
+      (recur (rest blocks)
+             (if (empty-space? (first blocks))
+               (rest moveable-blocks)
+               moveable-blocks)
+             (if (empty-space? (first blocks))
+               (conj final (first moveable-blocks))
+               (conj final (first blocks)))))))
 
-  (def block-index first)
-
-  (def result (loop [blocks          indexed-blocks
-                     moveable-blocks moveable-blocks
-                     final           []]
-                (if (or (empty? moveable-blocks)
-                        (< (block-index (first moveable-blocks))
-                           (block-index (first blocks))))
-                  final
-                  (recur (rest blocks)
-                         (if (empty-space? (first blocks))
-                           (rest moveable-blocks)
-                           moveable-blocks)
-                         (if (empty-space? (first blocks))
-                           (conj final (first moveable-blocks))
-                           (conj final (first blocks)))))))
-
-  (->> result
+(defn compute-checksum [defragmented-blocks]
+  (->> defragmented-blocks
        (map-indexed (fn [idx v] (vector idx (second v))))
        (map #(apply * %))
-       (reduce +))
+       (reduce +)))
 
+(defn solution-1 [input]
+  (->> input
+       build-blocks
+       build-indexed-blocks
+       defragment-indexed-blocks
+       compute-checksum))
+
+(comment
+
+  (solution-1 sample-input)
+  ;; => 1928 .. ok 
+
+  (solution-1 puzzle-input)
   ;; => 6446899523367 ⭐
 
-
-  ;;
+;;
   )
