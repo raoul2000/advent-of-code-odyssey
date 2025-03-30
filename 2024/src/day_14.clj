0a31d2429a909ac84741e3228c52125b5a6e08c8
raoul2000 <manu34@free.fr>
2025-03-30 13:38:37 +0200

    day_14 : just starting

---
+++ 2024/src/day_14.clj
@@ -0,0 +1,68 @@
+(ns day-14
+  (:require [clojure.string :as s]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+;; https://adventofcode.com/2024/day/14
+
+;; part 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; robot
+;; px,py : position of a robot
+;; vx,vy : velocity of a robot (in tiles per second)
+;; directions : 
+;; x -> x+1 right
+;;  y -> y+1 down
+;; top - left pos : 0,0
+;; grid
+;; col-count, row-count
+
+(def sample-input "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+")
+
+(def puzzle-input (slurp "resources/day_14.txt"))
+
+;; Data model
+;; Robot : 
+;;  {:px 37, :py 51, :vx 73, :vy 20}
+
+(defn parse-robot-line
+  "Given a string describing a robot, returns a map describing the same robot : 
+   
+   - `px` : X coordinates of robot position
+   - `py` : Y coordinates of robot position
+   - `vx` : horizontal velocity
+   - `vy` : vertical velocity
+   "
+  [line]
+  (->> (re-matches #"p=(\d+),(\d+) v=(-?\d+),(-?\d+)" line)
+       rest
+       (map edn/read-string)
+       (map #(vector %1 %2) [:px :py :vx :vy])
+       (into {})))
+
+(defn parse 
+  "Returns a seq of maps where each map describes a robot"
+  [input]
+  (->> input
+       str/split-lines
+       (map parse-robot-line)))
+
+(comment
+  
+  (parse sample-input)
+  (parse puzzle-input)
+
+  ;;
+  )
