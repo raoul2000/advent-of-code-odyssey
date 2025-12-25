9252cf319e3b31cb2ab2fa175789705fdf66b7c2
raoul2000 <manu34@free.fr>
2025-12-25 20:57:14 +0100

    day_3: still on part 1 🧑‍🎄🎄

--- 2025/src/day_3.clj
+++ 2025/src/day_3.clj
@@ -20,31 +20,48 @@
        (map (fn [bank]
               (map #(- (int %) 48) bank)))))
 
-(comment
+;; 1. build a seq of pairs [index num] 
+;; 2. take the first one as the first digit
+;;    find in rest, the first with an index value lower than the one just taken
+;;    save the 2 digits found as a number
+;;    go back to 2. with the rest of the pair seq
+;; 3. take the max number in the seq just built
+;;    
 
-  ;; given a seq of digit, get 2 max preserving order
+(comment
 
   (->> (parse sample-input)
        (map (fn [bank]
               (map-indexed (fn [k v] [k v]) bank)))
        (map (fn [indexed-bank]
               (sort-by second > indexed-bank)))
-       (map (fn [sorted-indexed-bank]
-              (take 2 sorted-indexed-bank)
-              ))
-       (map (fn [max2]
-              (sort-by first < max2)
-              ))
-       
        ;;
        )
 
-  (sort-by first (take 2 (sort-by second (->> (vec '(2 5 4 8))
-                                              (map-indexed (fn [k v] [k v]))))))
+  (def seq-of-pairs '([0 9] [1 8] [2 7] [3 6] [4 5] [5 4] [6 3] [7 2] [8 1] [9 1] [10 1] [11 1] [12 1] [13 1] [14 1]))
+  (def s2 '([14 9] [0 8] [1 1] [2 1] [3 1] [4 1] [5 1] [6 1] [7 1] [8 1] [9 1] [10 1] [11 1] [12 1] [13 1]))
+
+  (loop [pairs s2
+         results []]
+    (if (= 1 (count pairs))
+      results
+      (let [cur-idx (ffirst pairs)]
+        (recur (rest pairs)
+               (conj results [(first pairs)
+                              (->> (rest pairs)
+                                   (filter #(> (first %) cur-idx))
+                                   (sort-by first >)
+                                   first)])))))
+
+  (first (filter #(> (first %) 4) seq-of-pairs))
 
 
-  (sort > '(3 2 1))
-  (parse sample-input)
+
+
+
+
+
+
 
 
   ;;
